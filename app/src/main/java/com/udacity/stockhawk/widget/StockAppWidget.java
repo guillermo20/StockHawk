aea880ead61a77a316948530e07efc8c230fd69c
Guillermo Gutierrez <guillermo20@gmail.com>
2016-12-25 19:03:21 -0500

    Stock Widget almost done.

--- app/src/main/java/com/udacity/stockhawk/widget/StockAppWidget.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockAppWidget.java
@@ -1,11 +1,17 @@
 package com.udacity.stockhawk.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.ui.MainActivity;
+
+import static com.udacity.stockhawk.R.id.widget_empty;
+import static com.udacity.stockhawk.R.id.widget_list_view;
 
 /**
  * Implementation of App Widget functionality.
@@ -15,13 +21,35 @@ public class StockAppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        /*CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_app_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);*/
+
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_app_widget);
+
+        // launching main activity when clicking the bar button.
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        //views.setOnClickPendingIntent(R.id.wid,pendingIntent);
+        views.setRemoteAdapter(widget_list_view,
+                new Intent(context,
+                        StockWidgetRemoteViewsService.class));
+        views.setEmptyView(R.id.widget_list_view,R.id.widget_empty);
         appWidgetManager.updateAppWidget(appWidgetId, views);
+
+//        Cursor data = context.getContentResolver().query(Contract.Quote.uri,
+//                Contract.Quote.QUOTE_COLUMNS,
+//                null, null, Contract.Quote.COLUMN_SYMBOL);
+//        if (data == null){
+//            return;
+//        }
+//
+//        data.close();
     }
 
     @Override
---
+++ app/src/main/java/com/udacity/stockhawk/widget/StockWidgetRemoteViewsService.java
@@ -0,0 +1,106 @@
+package com.udacity.stockhawk.widget;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Binder;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * Created by guillermo on 12/25/16.
+ */
+
+public class StockWidgetRemoteViewsService extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsFactory() {
+            private Cursor data = null;
+            final private DecimalFormat dollarFormat = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+
+            @Override
+            public void onCreate() {
+
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data!=null){
+                    data.close();
+                }
+                final long identityToken = Binder.clearCallingIdentity();
+                data = getContentResolver().query(Contract.Quote.uri,
+                        null,
+                        null,
+                        null,
+                        null);
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+                RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.list_item_quote);
+
+                String symbol =  data.getString(Contract.Quote.POSITION_SYMBOL);
+                float price = data.getFloat(Contract.Quote.POSITION_PRICE);
+
+
+                float rawAbsoluteChange = data.getFloat(Contract.Quote.POSITION_ABSOLUTE_CHANGE);
+
+                views.setTextViewText(R.id.symbol,symbol);
+                views.setTextViewText(R.id.price,dollarFormat.format(price));
+
+                return views;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.list_item_quote);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                if (data.moveToPosition(i))
+                    return data.getLong(Contract.Quote.POSITION_ID);
+                return i;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+
+}
